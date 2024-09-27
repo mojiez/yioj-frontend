c76cf529ea7e73c51effe13c411de1cb90fb9761
mojiez <937691466@qq.com>
2024-09-27 18:36:33 +0800

    手写前端模版，完成最粗略的header content footer布局

---
+++ src/layouts/BasicLayout.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="basicLayout">
+    <a-layout style="height: 400px">
+      <a-layout-header class="header">导航栏</a-layout-header>
+      <a-layout-content class="content">
+        <router-view />
+      </a-layout-content>
+      <a-layout-footer class="footer">
+        <a href="https://www.baidu.com" target="_blank"> 无敌暴龙战神 </a>
+      </a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+<style scoped>
+#basicLayout {
+}
+
+#basicLayout .header {
+  background: red;
+  margin-bottom: 16px;
+}
+
+#basicLayout .content {
+  background: linear-gradient(to right, #bbb, #fff);
+  margin-bottom: 16px;
+}
+
+#basicLayout .footer {
+  background: #efefef;
+  padding: 16px;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  text-align: center;
+}
+</style>
