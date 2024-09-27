6d3ecb242a8cb1dd4d16860bbfb850d7bf0c6a00
mojiez <937691466@qq.com>
2024-09-27 16:21:46 +0800

    vue 整合 arco组件 测试成功

--- src/router/index.ts
+++ src/router/index.ts
@@ -16,6 +16,15 @@ const routes: Array<RouteRecordRaw> = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
   },
+  {
+    path: "/testarco",
+    name: "testarco",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/TestArcoView.vue"),
+  },
 ];
 
 const router = createRouter({
---
+++ src/views/TestArcoView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <a-steps :current="2">
+      <a-step>Succeeded</a-step>
+      <a-step>Processing</a-step>
+      <a-step>Pending</a-step>
+    </a-steps>
+    <a-divider />
+    <div style="line-height: 140px; text-align: center; color: #c9cdd4">
+      Step 2 Content
+    </div>
+  </div>
+</template>
