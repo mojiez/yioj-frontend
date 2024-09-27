74002da4544617532617f4b73b21855391257efd
mojiez <937691466@qq.com>
2024-09-27 19:37:15 +0800

    实现导航栏切换 content跟着切换， 并且可以根据route路径确定导航栏的key

--- src/components/GlobalHeader.vue
+++ src/components/GlobalHeader.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="globalHeader">
-    <a-menu mode="horizontal" :default-selected-keys="['1']">
+    <a-menu
+      mode="horizontal"
+      :selected-keys="selectedKeys"
+      @menu-item-click="doMenuClick"
+    >
       <a-menu-item
         key="0"
         :style="{ padding: 0, marginRight: '38px' }"
@@ -11,15 +15,31 @@
           <div class="title">易OJ</div>
         </div>
       </a-menu-item>
-      <a-menu-item key="1">Home</a-menu-item>
-      <a-menu-item key="2">Solution</a-menu-item>
-      <a-menu-item key="3">Cloud Service</a-menu-item>
-      <a-menu-item key="4">Cooperation</a-menu-item>
+      <a-menu-item v-for="item in routes" :key="item.path">{{
+        item.name
+      }}</a-menu-item>
     </a-menu>
   </div>
 </template>
 
-<script lang="ts"></script>
+<script setup lang="ts">
+import { routes } from "../router/routes";
+import { useRouter } from "vue-router";
+import { ref } from "vue";
+
+const router = useRouter();
+const selectedKeys = ref(["/"]);
+const doMenuClick = (key: string) => {
+  router.push({
+    path: key,
+  });
+};
+
+// 实现根据变化导航栏奇幻content内容， 同时能根据当前的路径反过来确定导航栏的key
+router.afterEach((to, from, failure) => {
+  selectedKeys.value = [to.path];
+});
+</script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
